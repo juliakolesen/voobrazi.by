ac048fa1330f66d01af0bf8004426bafb61b7288
scjaarge <alexander.metelsky@gmail.com>
2010-12-09 14:25:42 +0200

    "Not available" added for goods with empty price (quantity - 0)

--- NopCommerceStore/Modules/ProductBox1.ascx.designer.cs
+++ NopCommerceStore/Modules/ProductBox1.ascx.designer.cs
@@ -31,6 +31,24 @@ namespace NopSolutions.NopCommerce.Web.Modules {
         protected global::System.Web.UI.WebControls.HyperLink hlImageLink;
         
         /// <summary>
+        /// divProductInfoLink control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl divProductInfoLink;
+        
+        /// <summary>
+        /// notAvailable control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl notAvailable;
+        
+        /// <summary>
         /// hlProduct control.
         /// </summary>
         /// <remarks>
