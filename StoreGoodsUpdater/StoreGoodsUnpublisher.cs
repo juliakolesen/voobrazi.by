2abb6a9fb4d0317cbca2884155a9a6f558417162
scjaarge <alexander.metelsky@gmail.com>
2011-08-08 17:55:47 +0300

    Поправил распубликовывание товаров, которых нет на складе. Теперь распубликовывается не вариант продукта, а продукт

--- StoreGoodsUpdater/StoreGoodsUnpublisher.cs
+++ StoreGoodsUpdater/StoreGoodsUnpublisher.cs
@@ -1,5 +1,7 @@
 ﻿
 
+using NopSolutions.NopCommerce.BusinessLogic.Audit;
+
 namespace Scjaarge.NopTasks
 {
     using System;
@@ -11,11 +13,15 @@ namespace Scjaarge.NopTasks
 
     public class StoreGoodsUnpublisherTask : ITask
     {
-        private const string PublishQuery = @"UPDATE Nop_ProductVariant SET Published=1 WHERE StockQuantity > 0 OR
-	                                                ProductId IN (SELECT ProductID FROM Nop_Product_Category_Mapping WHERE CategoryId IN ({0}))";
-        private const string UnpublishQuery = @"UPDATE Nop_ProductVariant SET Published = 0 
-                                                WHERE StockQuantity = 0 AND 
-	                                                ProductId NOT IN (SELECT ProductID FROM Nop_Product_Category_Mapping WHERE CategoryId IN ({0}))";
+        private const string PublishProductQuery = @"UPDATE p SET p.Published=1 FROM Nop_Product p INNER JOIN Nop_ProductVariant pv 
+                                                        ON (p.ProductId = pv.ProductId 
+                                                            AND pv.StockQuantity > 0 
+                                                            AND p.ProductId IN (SELECT DISTINCT ProductID FROM Nop_Product_Category_Mapping WHERE CategoryId IN ({0})))";
+
+        private const string UnpublishProductQuery = @"UPDATE p SET p.Published = 0 FROM Nop_Product p INNER JOIN Nop_ProductVariant pv 
+                                                        ON (p.ProductId = pv.ProductId 
+                                                        AND pv.StockQuantity = 0
+    	                                                AND p.ProductId NOT IN (SELECT DISTINCT ProductID FROM Nop_Product_Category_Mapping WHERE CategoryId IN ({0})))";
 
         public void Execute(XmlNode node)
         {
@@ -24,21 +30,28 @@ namespace Scjaarge.NopTasks
             XmlAttribute categoryIDsToSkip = node.Attributes["CategoryIDsToSkip"];
             XmlAttribute connectionStringName = node.Attributes["ConnectionStringName"];
 
-            int tmpCategoryID;
             foreach (string categoryID in categoryIDsToSkip.Value.Split(";".ToCharArray(), StringSplitOptions.RemoveEmptyEntries))
             {
+                int tmpCategoryID;
                 if (!int.TryParse(categoryID, out tmpCategoryID))
                     return;
             }
 
-            using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings[connectionStringName.Value].ConnectionString))
+            try
             {
-                connection.Open();
+                using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings[connectionStringName.Value].ConnectionString))
+                {
+                    connection.Open();
 
-                using (var command = new SqlCommand(string.Format(PublishQuery, categoryIDsToSkip.Value.Replace(";", ",")), connection))
-                    command.ExecuteNonQuery();
-                using (var command = new SqlCommand(string.Format(UnpublishQuery, categoryIDsToSkip.Value.Replace(";", ",")), connection))
-                    command.ExecuteNonQuery();
+                    using (var command = new SqlCommand(string.Format(PublishProductQuery, categoryIDsToSkip.Value.Replace(";", ",")), connection))
+                        command.ExecuteNonQuery();
+                    using (var command = new SqlCommand(string.Format(UnpublishProductQuery, categoryIDsToSkip.Value.Replace(";", ",")), connection))
+                        command.ExecuteNonQuery();
+                }
+            }
+            catch (Exception exc)
+            {
+                LogManager.InsertLog(LogTypeEnum.AdministrationArea, "StoreGoodsUnpublisherTask exception.", exc);
             }
 
             NopCache.Clear();
