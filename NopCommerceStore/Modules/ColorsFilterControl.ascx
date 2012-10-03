75a614cecfc8aa0728b502154a89692d929c4b91
juliakolesen <julia.kolesen@gmail.com>
2012-10-03 20:47:06 +0300

    Colors Filter

---
+++ NopCommerceStore/Modules/ColorsFilterControl.ascx
@@ -0,0 +1,21 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="ColorsFilterControl.ascx.cs"
+    Inherits="NopSolutions.NopCommerce.Web.Modules.ColorsFilterControl" %>
+<%@ Register TagPrefix="nopCommerce" TagName="ColorCell" Src="~/Modules/ColorCell.ascx" %>
+<center>
+    <asp:Label ID="colorsLabel" runat="server" Text="Выбранный цвет:"></asp:Label>
+    <table>
+        <tr>
+            <td>
+                <asp:DataList ID="rptColors" runat="server" RepeatDirection="Horizontal">
+                    <ItemTemplate>
+                        <nopCommerce:ColorCell ID="cell" runat="server" ColorItem='<%# Container.DataItem %>' />
+                    </ItemTemplate>
+                </asp:DataList>
+            </td>
+            <td>
+                <asp:ImageButton ID="colorFilterDefault" runat="server" OnClick="onclick" ImageUrl="~/images/unchecked.gif"
+                    ToolTip="Фильтр по цветам неактивен" />
+            </td>
+        </tr>
+    </table>
+</center>
---
+++ NopCommerceStore/Modules/ColorsFilterControl.ascx.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using NopSolutions.NopCommerce.BusinessLogic.Categories;
+using NopSolutions.NopCommerce.BusinessLogic.Colors;
+using NopSolutions.NopCommerce.Common.Utils;
+using NopSolutions.NopCommerce.BusinessLogic.Products;
+
+namespace NopSolutions.NopCommerce.Web.Modules
+{
+    public partial class ColorsFilterControl : BaseNopUserControl
+    {
+        public List<ColorItem> Colors { get; set; }
+        public string CategoryName { get; set; }
+
+        protected string UrlToRedirect
+        {
+            get
+            {
+                string currentUrl = CommonHelper.GetThisPageURL(false);
+                List<string> parameters = new List<string>();
+                bool first = true;
+                for (int i = 0; i < Request.QueryString.Keys.Count; i++)
+                {
+                    string key = Request.QueryString.Keys[i];
+                    if (key != null && key != "Цвет" && key != "CategoryID")
+                    {
+                        currentUrl = String.Format("{0}{1}{2}={3}", currentUrl, first ? "?" : "&",
+                                            key, CommonHelper.QueryStringInt(key.ToString()));
+                        first = false;
+                    }
+                }
+                
+                return currentUrl;
+            }
+        }
+
+        protected override void OnPreRender(EventArgs e)
+        {
+            base.OnPreRender(e);
+            if (!String.IsNullOrEmpty(CategoryName) &&
+               ( CategoryName.Equals("живые цветы", StringComparison.CurrentCultureIgnoreCase)
+            || CategoryName.Equals("cвадебная флористика", StringComparison.CurrentCultureIgnoreCase)
+            || CategoryName.Equals("букеты и композиции", StringComparison.CurrentCultureIgnoreCase)))
+            {
+                BindData();
+            }
+            else
+            {
+                this.Visible = false;
+            }
+        }
+
+        protected void onclick(object sender, EventArgs e)
+        {
+           Response.Redirect(this.UrlToRedirect);
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void BindData()
+        {
+            if (Colors != null && Colors.Count > 0)
+            {
+                rptColors.DataSource = Colors;
+                rptColors.DataBind();
+            }
+            else
+            {
+                this.Visible = false;
+            }
+        }
+    }
+}
